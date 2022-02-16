816e100a78c6291e68a631dfb30eb97cea5ce5dd
caio <caio@qualityautomacao.com>
2022-02-16 09:45:24 -0300

    home

--- projects/v2/src/app/modules/home/home.module.ts
+++ projects/v2/src/app/modules/home/home.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home.routing.module';
-import { InputSearchModule } from '../../shared/input-search/input-search.module';
+import { InputSearchModule } from '../../shared/components/input-search/input-search.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
